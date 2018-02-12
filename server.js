547dc77186252f8e7bf6ef59405b8438d2cbb2a7
Jaime Rios <jaimemadrigalrios@gmail.com>
2018-02-11 20:11:45 -0600

    Added basic APIs.

--- server.js
+++ server.js
@@ -3,8 +3,26 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Express!');
+  // res.send('<h1>Hello Express!</h1>');
+  res.send({
+    name: 'Jaime',
+    likes: [
+      'Women',
+      'Protein powder',
+      'Movies',
+      'Tennis',
+    ],
+  });
  });
 
+app.get('/about', (req, res) => {
+  res.send('About Page');
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'unable to fulfill request',
+  });
+});
 
 app.listen(3000);
